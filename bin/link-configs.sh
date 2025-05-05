a4b417fe01a515cdaad1604d54424641d2addfe4
Nikos Polykandriotis <n.a.polikandriotis@gmail.com>
2025-05-05 20:06:45 +0300

    fix: correctly link folders

--- bin/link-configs.sh
+++ bin/link-configs.sh
@@ -6,9 +6,11 @@ ln -sf "$CURRENT_DIR/.zshrc" ~/.zshrc
 ln -sf "$CURRENT_DIR/.p10k.zsh" ~/.p10k.zsh
 ln -sf "$CURRENT_DIR/.aerospace.toml" ~/.aerospace.toml
 ln -sf "$CURRENT_DIR/.tmux.conf" ~/.tmux.conf
-ln -sf "$CURRENT_DIR/.config/ghostty" ~/.config/ghostty
+ln -sf "$CURRENT_DIR/.config/ghostty" ~/.config/
 ln -sf "$CURRENT_DIR/.config/nvim" ~/.config/
 ln -sf "$CURRENT_DIR/.config/sketchybar" ~/.config/
+
+# TMUX helper scripts
 ln -sf "$CURRENT_DIR/bin/tmux-yazi-floating-selector.sh"  /usr/local/bin/tmux-yazi-floating-selector.sh
 ln -sf "$CURRENT_DIR/bin/tmux-session-switcher.sh"  /usr/local/bin/tmux-session-switcher.sh
 ln -sf "$CURRENT_DIR/bin/show-tmux-popup.sh"  /usr/local/bin/show-tmux-popup.sh

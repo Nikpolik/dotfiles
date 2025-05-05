1f8aa4c786c468297ad56053d499347c710295df
Nikos Polykandriotis <n.a.polikandriotis@gmail.com>
2025-05-05 17:22:51 +0300

    feat: add tmux config

--- bin/link-configs.sh
+++ bin/link-configs.sh
@@ -4,6 +4,8 @@ mkdir -p ~/.config
 
 ln -sf "$CURRENT_DIR/.zshrc" ~/.zshrc
 ln -sf "$CURRENT_DIR/.p10k.zsh" ~/.p10k.zsh
-ln -sf "$CURRENT_DIR/.p10k.zsh" ~/.p10k.zsh
+ln -sf "$CURRENT_DIR/.aerospace.toml" ~/.aerospace.toml
+ln -sf "$CURRENT_DIR/.tmux.conf" ~/.tmux.conf
 ln -sf "$CURRENT_DIR/.config/ghostty" ~/.config/ghostty
 ln -sf "$CURRENT_DIR/.config/nvim" ~/.config/nvim
+ln -sf "$CURRENT_DIR/.config/sketchybar" ~/.config/sketchybar

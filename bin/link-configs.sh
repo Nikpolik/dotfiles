3dfdc3284fa1eedc9bf935f80fe52f81975ccdbd
nikpolik <n.a.polikandriotis@gmail.com>
2025-08-21 22:07:47 +0300

    feat: move executables to ~/bin

--- bin/link-configs.sh
+++ bin/link-configs.sh
@@ -1,6 +1,7 @@
 CURRENT_DIR=$(pwd)
 
 mkdir -p ~/.config
+mkdir -p ~/bin
 
 ln -sf "$CURRENT_DIR/.zshrc" ~/.zshrc
 ln -sf "$CURRENT_DIR/.p10k.zsh" ~/.p10k.zsh
@@ -12,7 +13,7 @@ ln -sf "$CURRENT_DIR/.config/sketchybar" ~/.config/
 ln -sf "$CURRENT_DIR/.config/yazi" ~/.config/
 
 # TMUX helper scripts
-ln -sf "$CURRENT_DIR/bin/tmux/yazi-floating-selector.sh"  /usr/local/bin/tmux-yazi-floating-selector.sh
-ln -sf "$CURRENT_DIR/bin/tmux/session-switcher.sh"  /usr/local/bin/tmux-session-switcher.sh
-ln -sf "$CURRENT_DIR/bin/tmux/show-popup.sh"  /usr/local/bin/tmux-show-popup.sh
-ln -sf "$CURRENT_DIR/bin/tmux/open-in-tmux.sh"  /usr/local/bin/tmux-open-in-tmux.sh
+ln -sf "$CURRENT_DIR/bin/tmux/yazi-floating-selector.sh"  ~/bin/tmux-yazi-floating-selector.sh
+ln -sf "$CURRENT_DIR/bin/tmux/session-switcher.sh"  ~/bin/tmux-session-switcher.sh
+ln -sf "$CURRENT_DIR/bin/tmux/show-popup.sh"  ~/bin/tmux-show-popup.sh
+ln -sf "$CURRENT_DIR/bin/tmux/open-in-tmux.sh"  ~/bin/tmux-open-in-tmux.sh

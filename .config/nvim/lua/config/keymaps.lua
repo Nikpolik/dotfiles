763ac1efb8120a3718fea87b67811d5318cd5aa3
nikpolik <n.a.polikandriotis@gmail.com>
2026-01-07 22:48:50 +0200

    chore(vim): add keybind to copy repository path for current file

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,4 +4,9 @@ wk.add({
   { "<leader>h",  "<CMD>noh<CR>",                             desc = "Clear highlight" },
   { "<leader>bb", "<CMD>b#<CR>",                              desc = "Prev buff" },
   { "gl",         "<CMD>lua vim.diagnostic.open_float()<CR>", desc = "Get line diagnostics" },
+  { "<leader>yp", function()
+      local relative_path = vim.fn.expand('%:p:.')
+      vim.fn.setreg('+', relative_path)
+      vim.notify('Copied: ' .. relative_path, vim.log.levels.INFO)
+    end, desc = "Copy cwd-relative path" },
 })
